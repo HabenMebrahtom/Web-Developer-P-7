e7dd1daac06ea7658d2d08117e12172efe9e2237
HabenMebrahtom <d_haben10@yahoo.com>
2022-08-19 00:46:13 +0200

    updated

--- groupomania/src/components/Register.js
+++ groupomania/src/components/Register.js
@@ -53,11 +53,8 @@ function Register() {
                     <div className="card-body p-md-5">
                         <div className="row justify-content-center">
                         <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-
                             <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-
                             <form className="mx-1 mx-md-4">
-
                             <div className="d-flex flex-row align-items-center mb-4">
                                 <div className="form-outline flex-fill mb-0">
                                 <label className="form-label" htmlFor="fullname">Your Name</label>                              
